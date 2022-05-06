7f0e8f9380281ab241bc6e13c10b32824599f30d
Mehedi Mazumder <mehedirumc12cuet14@gmail.com>
2022-05-06 23:54:18 +0600

    Time: 12 ms (35.98%), Space: 95 MB (29.33%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -22,4 +22,30 @@ class Solution {
         
         return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
     }
+    
+    
+        // public int minDepth(TreeNode root) {
+        // if(root == null) return 0;
+        // int depth = 1;
+        // Queue<TreeNode> q = new LinkedList<TreeNode>();
+        // q.offer(root);
+        // while(!q.isEmpty()){
+        //     int size = q.size();
+        //     // for each level
+        //     for(int i=0;i<size;i++){
+        //         TreeNode node = q.poll();
+        //         if(node.left == null && node.right == null){
+        //             return depth;
+        //         }
+        //         if(node.left != null){
+        //             q.offer(node.left);
+        //         }
+        //         if(node.right != null){
+        //             q.offer(node.right);
+        //         }
+        //     }
+        //     depth++;
+        // }
+        // return depth;
+        // }
 }
