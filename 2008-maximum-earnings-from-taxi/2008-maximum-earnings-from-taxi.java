6ba9bf3e6d5af0b50eaa64e818a7313bcea73d94
Mehedi Mazumder <mehedirumc12cuet14@gmail.com>
2022-05-20 00:14:46 +0600

    Time: 138 ms (40.99%), Space: 132.7 MB (17.44%) - LeetHub

---
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.java
@@ -0,0 +1,30 @@
+class Solution {
+    public long maxTaxiEarnings(int n, int[][] rides) {
+        Map<Integer, List<int[]>> trips = new HashMap<>();
+        
+        for(int[] ride: rides){
+            if(trips.containsKey(ride[1])){
+                trips.get(ride[1]).add(ride);
+            }else{
+                trips.put(ride[1], new ArrayList<>(Arrays.asList(ride)));
+            }
+        }
+                        
+       long[] dp = new long[n+1];  
+       
+       for(int i = 2; i <= n; i++){
+           List<int[]> values = trips.get(i);
+           
+           if(values != null){
+               long earning = 0;
+               
+               for(int[] trip : values){
+                   earning = Math.max(earning, trip[1] - trip[0] + trip[2] + dp[trip[0]]);
+               }
+               dp[i] = earning;
+           }
+           dp[i] = Math.max(dp[i], dp[i-1]);
+       }
+        return dp[n];
+    }
+}
