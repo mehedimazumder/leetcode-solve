a96605f32c7aab1ab4c7a73fb768fbd598af7fdf
Mehedi Mazumder <mehedirumc12cuet14@gmail.com>
2022-05-20 13:08:09 +0600

    Update 3-longest-substring-without-repeating-characters.java

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -20,4 +20,26 @@ class Solution {
         }
         return maxCount;
     }
+    
+    // O(N)
+     public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        int i = 0;
+        int j = 0;
+        
+        int maxSub = 0;
+        while(j < s.length()){
+            Integer repeatedCharIdx = map.get(s.charAt(j));
+            
+            if(repeatedCharIdx == null || (i >= repeatedCharIdx && j <= repeatedCharIdx)){
+                map.put(Character.valueOf(s.charAt(j)), j);
+                maxSub = Math.max(maxSub, j -i + 1);
+            }else{
+                i = repeatedCharIdx + 1;
+            }
+            j++;
+        }
+        return maxSub;
+    }
 }
